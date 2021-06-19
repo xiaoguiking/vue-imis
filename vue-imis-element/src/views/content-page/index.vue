6bedcfa66709e6d47006df7e1e7fd3565c736252
lihuazhi <lihuazhi@xiaodutv.com>
2021-06-19 19:47:31 +0800

    wiip: content page list

--- vue-imis-element/src/views/content-page/index.vue
+++ vue-imis-element/src/views/content-page/index.vue
@@ -61,11 +61,12 @@
         :row-class-name="tableRowClassName"
         ref="multipleTable"
         @selection-change="handleSelectionChange"
+        :default-sort = "{prop: 'date', order: 'descending'}"
       >
         <el-table-column type="selection" width="55"> </el-table-column>
-        <el-table-column prop="date" label="日期" width="180">
+        <el-table-column prop="date" label="日期1111" width="180" sortable>
         </el-table-column>
-        <el-table-column prop="name" label="姓名" width="180">
+        <el-table-column prop="name" label="姓名" width="180" sortable>
         </el-table-column>
         <el-table-column prop="address" label="地址"> </el-table-column>
         <el-table-column prop="city" label="城市"> </el-table-column>
