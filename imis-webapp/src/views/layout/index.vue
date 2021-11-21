06c504ab4590e894f82cf5b69526619f6b2709f7
xiaoguiking <king1129371217@163.com>
2021-11-21 21:22:35 +0800

    style: my done

--- imis-webapp/src/views/layout/index.vue
+++ imis-webapp/src/views/layout/index.vue
@@ -3,12 +3,12 @@
     <!-- 子路由 -->
     <router-view></router-view>
     <!-- 底部导航 -->
-    <!-- <van-tabbar v-model="active" route>
+    <van-tabbar v-model="active" route>
       <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
       <van-tabbar-item to="/video" icon="tv-o">视频</van-tabbar-item>
       <van-tabbar-item to="/qa" icon="friends-o">问答</van-tabbar-item>
       <van-tabbar-item to="/my" icon="user-o">我的</van-tabbar-item>
-    </van-tabbar> -->
+    </van-tabbar>
   </div>
 </template>
 
--- imis-webapp/src/views/my/index.vue
+++ imis-webapp/src/views/my/index.vue
@@ -1,5 +1,53 @@
 <template>
-  <div>my page</div>
+  <div class="my-container">
+    <van-cell-group class="my-info">
+      <van-cell title="昵称" value="昵称" center class="base-info" :border="false">
+        <van-image
+          round
+          class="avatar"
+          width="30"
+          height="30"
+          slot="icon"
+          src="https://img01.yzcdn.cn/vant/cat.jpeg"
+        />
+        <div slot="title">昵称</div>
+        <van-button type="primary" size="mini" round class="update-btn">编辑资料</van-button>
+      </van-cell>
+      <van-grid :border="false" class="base-grid">
+        <van-grid-item>
+          <div slot="text">
+            <span slot="icon">123</span>
+            <div class="text">头条</div>
+          </div>
+        </van-grid-item>
+        <van-grid-item>
+          <div slot="text">
+            <span slot="icon">123</span>
+            <div class="text">头条</div>
+          </div>
+        </van-grid-item>
+        <van-grid-item>
+          <div slot="text">
+            <span slot="icon">123</span>
+            <div class="text">头条</div>
+          </div>
+        </van-grid-item>
+        <van-grid-item>
+          <div slot="text">
+            <span slot="icon">123</span>
+            <div class="text">头条</div>
+          </div>
+        </van-grid-item>
+      </van-grid>
+      <van-grid :column-num="2">
+        <van-grid-item icon="star-o" text="收藏" />
+        <van-grid-item icon="browsing-history-o" text="历史" />
+      </van-grid>
+      <van-cell title="消息通知" is-link url="/vant/mobile.html" />
+      <van-cell title="小爱同学" is-link to="index" />
+      <van-cell title="退出登录" />
+    </van-cell-group>
+  </div>
 </template>
 
 <script>
@@ -8,4 +56,20 @@ export default {
 }
 </script>
 
-<style lang="name" scoped></style>
+<style lang="less">
+.my-container {
+  .my-info {
+    background: url('../../assets/img/banner.jpg') no-repeat;
+    opacity: 0.9;
+    background-size: contain;
+    .base-info {
+      background: unset;
+    }
+    .base-grid {
+      .van-grid-item__content {
+        background-color: unset;
+      }
+    }
+  }
+}
+</style>
