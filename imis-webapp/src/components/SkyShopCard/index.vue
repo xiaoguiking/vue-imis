e75c64288aa6cf3eafc90266d7c931233d142a90
lihuazhi <lihuazhi@xiaodutv.com>
2021-12-14 18:47:06 +0800

    webapp-imis: order-details

--- imis-webapp/src/components/SkyShopCard/index.vue
+++ imis-webapp/src/components/SkyShopCard/index.vue
@@ -9,13 +9,15 @@
       <div class="sky-shop-content font-md">
         <div class="sky-shop-multi-ellipsis--l2 sky-ellipsis">{{ title }}</div>
         <div class="sky-shop-car-desc mt-5">描述:{{ desc }}</div>
-        <div class="sky-shop-price mt-10">￥{{ price }}</div>
+        <div class="sky-shop-bottom mt-10">
+          <div class="sky-shop-price">￥{{ price }}</div>
+          <div class="sky-shop_num">{{ num }}</div>
+        </div>
       </div>
     </div>
     <div class="sky-shop-operate d-flex jc-end">
       <sky-button type="init">售后中</sky-button>
     </div>
-    <div class="sky-shop-footer">信息</div>
   </div>
 </template>
 
@@ -36,6 +38,9 @@ export default {
     },
     thumb: {
       type: String
+    },
+    num: {
+      type: String
     }
   },
   created() {
@@ -46,9 +51,11 @@ export default {
 
 <style lang="less">
 .sky-shop-car {
-  margin: 10px;
+  margin: 0 10px;
   border-radius: 5px;
   background-color: #fff;
+  border-bottom: 1px solid #ccc;
+  padding: 5px;
   .sky-shop-header {
     .sky-shop-image {
       position: relative;
@@ -81,6 +88,13 @@ export default {
       }
       .sky-shop-price {
         font-weight: 700;
+      }
+      .sky-shop-bottom {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        // padding-left: 5px;
+        padding-right: 10px;
       }
     }
   }
--- imis-webapp/src/views/order-details/index.vue
+++ imis-webapp/src/views/order-details/index.vue
@@ -6,12 +6,39 @@
       :mobile="menuList.mobile"
       :address="menuList.address"
     ></sky-card>
-    <sky-shop-card
+    <!-- <sky-shop-card
       price="2.00"
       desc="8GB+256GB"
       title="Redmi K40骁龙870三星AMOLED 120Hz高刷直屏小米红米 晴雪 游戏电竞智能5G 4800万高清三摄相机"
       thumb="https://img01.yzcdn.cn/vant/ipad.jpeg"
+    ></sky-shop-card> -->
+    <sky-shop-card
+      v-for="(item, index) of shopCar"
+      :price="item.price"
+      :desc="item.desc"
+      :title="item.title"
+      :thumb="item.thumb"
+      :key="index"
+      :num="item.number"
     ></sky-shop-card>
+    <div class="sky-shop-info">
+      <div class="info-item">
+        <span>运费</span>
+        <span>123</span>
+      </div>
+      <div class="info-item">
+        <span>订单总额</span>
+        <span>123</span>
+      </div>
+      <div class="info-item">
+        <span>优惠券折扣</span>
+        <span>123</span>
+      </div>
+      <div class="info-item">
+        <span>应付款</span>
+        <span>123</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -28,9 +55,17 @@ export default {
       shopCar: [
         {
           price: '2.00',
-          desc: '描述信息',
+          desc: '无描述',
           title: '商品标题',
-          thumb: 'https://img01.yzcdn.cn/vant/ipad.jpeg'
+          thumb: 'https://img01.yzcdn.cn/vant/ipad.jpeg',
+          number: '1'
+        },
+        {
+          price: '2.00',
+          desc: '我是东山啊',
+          title: '手机',
+          thumb: 'https://img01.yzcdn.cn/vant/ipad.jpeg',
+          number: '1'
         }
       ]
     }
@@ -48,6 +83,30 @@ export default {
   */
   .border-bottom {
     border: 0;
+  }
+  .sky-shop-info {
+    display: flex;
+    flex-direction: column; // align-items: center;
+    font-size: 18px;
+    background-color: #fff;
+    margin: 0 10px;
+    .info-item {
+      display: flex;
+      justify-content: space-between;
+      padding: 5px;
+      &:nth-child(1) {
+        color: gray;
+      }
+      &:nth-child(2) {
+        color: gray;
+      }
+      &:nth-child(3) {
+        color: gray;
+      }
+      &:nth-child(4) {
+        font-weight: 700;
+      }
+    }
   }
 }
 </style>
